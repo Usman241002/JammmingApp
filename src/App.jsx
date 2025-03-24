e4e399d22be41fa567936854f44884c6710f2566
Usman Khalid <ukhalid428@gmail.com>
2025-03-24 20:10:42 +0000

    Refined the hero section, and now working on making the results and playlist section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import Main from "./components/Main/Main"
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import '../../styles/Main.css';
 
 export default function Hero() {
   return(
-    <>
-    </>
+    <main className = "flexContainer">
+      <section id = "results" className = "flexContainer">
+
+      </section>
+
+      <section id = "playlist" className = "flexContainer">
+
+      </section>
+    </main>
   );
 }
